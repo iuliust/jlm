f64198254f5d83310937c5c2c23ea5dd90c6c55b
iuliust <iuliust@gmail.com>
2016-10-15 22:55:22 +0200

    ajout d'espaces insécables dans les expressions entre guillemets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Programme de Jxxn-Lxc Mxxxxxxxxn';
+  title = 'Programme de Jean-Luc Mélenchon';
 }
