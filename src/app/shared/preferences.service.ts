7ef54261f60897891fc1b6f78986c98d50d79b63
iuliust <iuliust@gmail.com>
2016-10-22 03:16:38 +0200

    ajoute des tests au service de chapitre

---
+++ src/app/shared/preferences.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PreferencesService {
+	fontSize: number;
+
+	constructor() {
+		this.fontSize = 1;
+	}
+
+	incrementFontSize() {
+		this.fontSize += 0.1;
+	}
+
+	decrementFontSize() {
+		this.fontSize -= 0.1;
+	}
+
+}
