86f63e97baa14bfc5543373e9ff8bdd2d1d12140
iuliust <iuliust@gmail.com>
2016-10-30 22:44:30 +0100

    refactorise les routes change l'affichage de la goutière de gauche dans l'affichage des chapitres corrige l'affichage du sidenav ChapitrePageComponent : style le bandeau de modification de la taille des polices vide EncartComponent de son contenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ChapitrePageComponent, ChapitreComponent } from './chapitre';
 import { HomeComponent } from './home/home.component';
 import { EncartFiltresComponent } from './encart-filtres/encart-filtres.component';
 
+export const routes: Routes = [
+		{ path: '', component: HomeComponent },
+		{
+			path: 'chapitres',
+			children: [
+				{ path: '', component: ChapitreComponent },
+				{
+					path: ':chapitreId',
+					children: [
+						{ path: '', component: EncartFiltresComponent, outlet: 'encart' },
+						{ path: '', component: ChapitrePageComponent }
+					]
+				}
+			]
+		}
+	];
+
 @NgModule({
 	imports: [
-		RouterModule.forRoot([
-			{ path: '', component: HomeComponent },
-			{
-				path: 'chapitres',
-				children: [
-					{ path: '', component: ChapitreComponent },
-					{
-						path: ':chapitreId',
-						children: [
-							{ path: '', component: EncartFiltresComponent, outlet: 'encart' },
-							{ path: '', component: ChapitrePageComponent }
-						]
-					}
-				]
-			}
-		])
+		RouterModule.forRoot(routes)
 	],
 	exports: [
 		RouterModule
