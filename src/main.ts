97473f3f00b2bd2451ced3f13d8592ff37ffef97
Julien Teyteau <julien.teyteau@gmail.com>
2016-12-04 05:30:47 +0100

    le hot module reloading est presque prêt

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,27 @@ import 'hammerjs';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode, CompilerOptions, ViewEncapsulation } from '@angular/core';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 import { AppModule } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-// let options: CompilerOptions = {
-//   defaultEncapsulation: ViewEncapsulation.Emulated
-// };
+const bootstrap = () => {
+  return platformBrowserDynamic().bootstrapModule(AppModule);
+};
+
+
+if (environment.hmr) {
+  if (module[ 'hot' ]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
